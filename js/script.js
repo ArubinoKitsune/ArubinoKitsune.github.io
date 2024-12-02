daca66a43e60b7450b9d0c6e2528ffe27d31c6be
ArubinoKitsune <lisiasty.poczta.pl@gmail.com>
2024-12-02 20:19:16 +0100

    chowane aktualności

--- js/script.js
+++ js/script.js
@@ -5,3 +5,33 @@ function changeLogo() {
 function resetLogo() {
     document.getElementById("logo").src = "arts/logo.png"; // Change back to the original image
 }
+var z = 0;
+$(document).ready(function() {
+$(".arrow").on( "click", function() {
+        $("#aktualnosci").slideToggle();
+        if(z==0){
+            $('#arrow'). animate(
+                { deg: 90 },
+                {
+                duration: 120,
+                step: function(now) {
+                $(this). css({ transform: 'rotate(' + now + 'deg)' });
+                z=1;
+                }
+            });
+            
+        }
+        if(z==1){
+            $('#arrow'). animate(
+                { deg: -90 },
+                {
+                duration: 120,
+                step: function(now) {
+                $(this). css({ transform: 'rotate(' + now + 'deg)' });
+                }
+            });
+            z=0;
+        }
+        
+  } );
+});
