e763273df98fdbd65378dd4105c1652499f40074
ArubinoKitsune <lisiasty.poczta.pl@gmail.com>
2024-12-02 18:36:07 +0100

    ruchome logo, zwijanie
    początek zwijania aktualności

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function changeLogo() {
+    document.getElementById("logo").src = "arts/logo.gif"; // Change to the second image
+}
+
+function resetLogo() {
+    document.getElementById("logo").src = "arts/logo.png"; // Change back to the original image
+}
